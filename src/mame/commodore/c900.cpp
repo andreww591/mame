eb2e7214cef03bdd03db49a61e1307811ada2c37
Andrew Warkentin <andreww591@gmail.com>
2026-09-20 23:19:16 -0600

    feat: hooked up MMU on C900 and corrected RAM physical address (it was at 0x8000, but should be at 0x80000)

--- src/mame/commodore/c900.cpp
+++ src/mame/commodore/c900.cpp
@@ -33,13 +33,15 @@ To Do:
 #include "emu.h"
 #include "cpu/z8000/z8000.h"
 #include "cpu/m6502/m6510.h"
+#include "machine/bankdev.h"
 #include "machine/z80scc.h"
+#include "machine/z8010.h"
 #include "bus/rs232/rs232.h"
 #include "machine/z8536.h"
 #include "sound/spkrdev.h"
 #include "emupal.h"
 #include "speaker.h"
-
+#include "logmacro.h"
 
 namespace {
 
@@ -49,6 +51,8 @@ public:
 	c900_state(const machine_config &mconfig, device_type type, const char *tag)
 		: driver_device(mconfig, type, tag)
 		, m_maincpu(*this, "maincpu")
+		, m_mmu(*this, "mmu")
+		, m_physmem(*this, "physmem")
 		, m_fdcpu(*this, "fdcpu")
 		, m_spkrdev(*this, "speaker")
 	{ }
@@ -61,12 +65,25 @@ private:
 	void data_map(address_map &map) ATTR_COLD;
 	void io_map(address_map &map) ATTR_COLD;
 	void special_io_map(address_map &map) ATTR_COLD;
-	void mem_map(address_map &map) ATTR_COLD;
 	void fdc_map(address_map &map) ATTR_COLD;
+	void phys_mem_map(address_map &map);
+	void virt_mem_map(address_map &map);
+	void virt_data_map(address_map &map);
+	bool translate_addr(int spacenum, bool write, offs_t &offset);
+	uint16_t virt_r(address_space &space, offs_t offset, uint16_t mask);
+	void virt_w(address_space &space, offs_t offset, uint16_t data, uint16_t mask);
+	uint16_t segtack_r();
+	void segt_interrupt(int state);
+	uint8_t mmu_read(offs_t offset);
+	void mmu_write(offs_t offset, uint8_t data);
 
-	required_device<cpu_device> m_maincpu;
+
+	required_device<z8001_device> m_maincpu;
+	required_device<z8010_device> m_mmu;
+	required_device<address_map_bank_device> m_physmem;
 	required_device<cpu_device> m_fdcpu;
 	required_device<speaker_sound_device> m_spkrdev;
+	bool m_dma_on = false;
 };
 
 void c900_state::sound_pb_w(u8 data)
@@ -74,16 +91,69 @@ void c900_state::sound_pb_w(u8 data)
 	m_spkrdev->level_w(BIT(data, 0));
 }
 
-void c900_state::mem_map(address_map &map)
+void c900_state::phys_mem_map(address_map &map)
 {
 	map(0x000000, 0x007fff).rom().region("roms", 0);
+	map(0x080000, 0x0fffff).ram();
+	//map(0x3f0000, 0x3f1fff).ram();
 }
 
-void c900_state::data_map(address_map &map)
+#define NON_MMU_MASK 0x1ff
+
+bool c900_state::translate_addr(int spacenum, bool write, offs_t &offset)
 {
-	map(0x000000, 0x007fff).rom().region("roms", 0);
-	map(0x008000, 0x06ffff).ram();
-	map(0x3f0000, 0x3f1fff).ram();
+	bool stack_access = (spacenum == z8001_device::AS_STACK);
+
+	//offset <<= 1;
+
+	offs_t mmu_offset = offset & ~NON_MMU_MASK;
+
+	bool code_access = (spacenum == AS_PROGRAM);
+	int st = code_access ?
+				(m_maincpu->is_ifetch1() ?
+					z8002_device::ST_IFETCH_1 :
+					z8002_device::ST_IFETCH_N) :
+				(stack_access ?
+					z8002_device::ST_REQ_STACK :
+					z8002_device::ST_REQ_DATA);
+
+	LOG("%s MMU MEM REQ (space %d): %06x\n", machine().describe_context(), spacenum, offset);
+
+	offset &= 0x3f'ffff;	// Mask off seg bit 7 to disable URS checking in MMUs
+
+	if (!m_mmu->translate(mmu_offset, write, true, m_dma_on, st)){
+		mmu_offset = 0;
+	}
+	offset = mmu_offset | (offset & NON_MMU_MASK);
+	return true;
+}
+
+
+uint16_t c900_state::virt_r(address_space &space, offs_t offset, uint16_t mask)
+{
+    uint16_t data = 0;
+    if (translate_addr(space.spacenum(), false, offset))
+        data = m_physmem->read16(offset, mask);
+	else if (space.spacenum() == AS_PROGRAM)
+        data = 0x8d07;  // NOP instruction
+
+    return data;
+}
+
+void c900_state::virt_w(address_space &space, offs_t offset, uint16_t data, uint16_t mask)
+{
+    if (translate_addr(space.spacenum(), true, offset))
+        m_physmem->write16(offset, data, mask);
+}
+
+void c900_state::virt_mem_map(address_map &map)
+{
+	map(0x00'0000, 0x7f'ffff).rw(FUNC(c900_state::virt_r), FUNC(c900_state::virt_w));
+}
+
+void c900_state::virt_data_map(address_map &map)
+{
+	map(0x00'0000, 0x7f'ffff).rw(FUNC(c900_state::virt_r), FUNC(c900_state::virt_w));
 }
 
 void c900_state::io_map(address_map &map)
@@ -94,13 +164,52 @@ void c900_state::io_map(address_map &map)
 
 void c900_state::special_io_map(address_map &map)
 {
-	// TODO: Z8010 MMU
+	map(0x0000, 0xffff).rw(FUNC(c900_state::mmu_read), FUNC(c900_state::mmu_write));
+}
+
+#define MMU1  0xfc
+#define MMU12 0xf8
+bool mmu_valid(offs_t offset) {
+	offs_t base = (offset & 0x00ff);
+	return (base == MMU1 || base == MMU12);
+}
+
+uint8_t c900_state::mmu_read(offs_t offset)
+{
+	if (mmu_valid(offset)) {
+		return (m_mmu->read(offset >> 8));
+	}else{
+		return (0);
+	}
+}
+
+void c900_state::mmu_write(offs_t offset, uint8_t data)
+{
+	if (mmu_valid(offset)) {
+		m_mmu->write(offset >> 8, data);
+	}else{
+		return ;
+	}
 }
 
 void c900_state::fdc_map(address_map &map)
 {
 	map(0x0000, 0x01ff).noprw(); // internal
 	map(0xe000, 0xffff).rom().region("fdc", 0);
+}
+
+uint16_t c900_state::segtack_r()
+{
+	uint16_t code = m_mmu->segtack_r();
+	if (code == 0) {
+		segt_interrupt(0);
+	}
+	return code;
+}
+
+void c900_state::segt_interrupt(int state)
+{
+	m_maincpu->set_input_line(z8001_device::SEGT_LINE, state ? ASSERT_LINE : CLEAR_LINE);
 }
 
 static INPUT_PORTS_START( c900 )
@@ -128,10 +237,16 @@ void c900_state::c900(machine_config &config)
 {
 	/* basic machine hardware */
 	Z8001(config, m_maincpu, 12_MHz_XTAL / 2);
-	m_maincpu->set_addrmap(AS_PROGRAM, &c900_state::mem_map);
-	m_maincpu->set_addrmap(AS_DATA, &c900_state::data_map);
+	m_maincpu->set_addrmap(AS_PROGRAM, &c900_state::virt_mem_map);
+	m_maincpu->set_addrmap(AS_DATA, &c900_state::virt_data_map);
 	m_maincpu->set_addrmap(AS_IO, &c900_state::io_map);
 	m_maincpu->set_addrmap(z8001_device::AS_SIO, &c900_state::special_io_map);
+	m_maincpu->segtack().set(FUNC(c900_state::segtack_r));
+
+	ADDRESS_MAP_BANK(config, m_physmem).set_map(&c900_state::phys_mem_map).set_options(ENDIANNESS_BIG, 16, 23, 0x100'0000);
+
+	Z8010(config, m_mmu, 12_MHz_XTAL / 2);
+	m_mmu->out_segt_cb().set(FUNC(c900_state::segt_interrupt));
 
 	M6508(config, m_fdcpu, 12_MHz_XTAL / 8); // PH1/PH2 = 1.5 MHz
 	m_fdcpu->set_addrmap(AS_PROGRAM, &c900_state::fdc_map);
